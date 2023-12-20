29950b38600a4503fba69b42ab18fbc37cf509d3
ajeeq <haziqazfar041@gmail.com>
2023-12-20 19:22:39 +0800

    feat: added dummy data

--- src/app/(BottomTab)/SecondaryScreen.tsx
+++ src/app/(BottomTab)/SecondaryScreen.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-
 import {
   View,
   Text,
-  Box
+  Box,
+  HStack,
+  VStack,
+  ScrollView,
 } from '@gluestack-ui/themed';
+
+import data from '@/data/122023.json';
 
 export default function SecondaryScreen() {
   const insets = useSafeAreaInsets();
@@ -15,10 +19,43 @@ export default function SecondaryScreen() {
     <View flex={1} alignItems="center" style={{
       paddingTop: insets.top
     }}>
-      <Box flex={1} w="$full" px="$4">
-        <Text>Monthly expense sums up</Text>
+      <Box flex={1} w="$full">
+        <Box px="$4">
+          <Text>Monthly expense sums up</Text>
+        </Box>
+        
+        <ScrollView px="$4">
+        {data.days.map((d) => (
+          <Box
+            borderRadius={4}
+            borderWidth={1}
+            mt="$4"
+          >
+            <VStack flex={1} p="$4">
+              <Text>{d.date}</Text>
+              {d.items.map((item) => (
+                <HStack justifyContent="space-between">
+                  <Text>{item.item_name}</Text>
+                  <Text>RM{item.item_price}</Text>
+                </HStack>
+              ))}
+            </VStack>
+          </Box>
+        ))}
+        </ScrollView>
+
+        <VStack 
+          bgColor="white"
+          w="$full"
+        >
+          <Box px="$4" pt="$2.5" mb="$5">
+            <HStack justifyContent="space-between">
+              <Text>Expenses so far</Text>
+              <Text>RM539.20</Text>
+            </HStack>
+          </Box>
+        </VStack>
       </Box>
-      <StatusBar style="auto" />
     </View>
   );
 }
